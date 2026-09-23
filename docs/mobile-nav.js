712fbb795f32bc23e8b8e10bb2aac71ac9660c70
Kofge <zerodo91@gmail.com>
2026-09-23 23:02:46 +0300

    Update website release fallback to v1.0.3

--- docs/mobile-nav.js
+++ docs/mobile-nav.js
@@ -1,5 +1,5 @@
 (() => {
-  const FALLBACK_VERSION = 'v0.19.5';
+  const FALLBACK_VERSION = 'v1.0.3';
   const RELEASE_URL = 'https://github.com/Kofge1/Kofge-Clicker/releases/latest';
   const RELEASE_API = 'https://api.github.com/repos/Kofge1/Kofge-Clicker/releases/latest';
   const RELEASE_CACHE_KEY = 'kofge-latest-release-v1';
